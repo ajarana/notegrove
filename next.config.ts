b5f889d6a62d6c347395a0daeac0f73e06c6870c
Andres <andresredans@gmail.com>
2025-05-29 02:16:57 -0400

    Dev server config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const LOCAL_IPV4 = process.env.LOCAL_IPV4 ? process.env.LOCAL_IPV4 : "";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [LOCAL_IPV4],
 };
 
 export default nextConfig;
